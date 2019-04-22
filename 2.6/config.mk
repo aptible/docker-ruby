fb2c0225e7442871cfb1c9c1a71cd02ef9488bad
Alex Kubacki <alex@aptible.com>
2019-04-22 13:13:22 -0400

    Version bump for 2.6

--- 2.6/config.mk
+++ 2.6/config.mk
@@ -1,6 +1,6 @@
 export RUBY_MAJOR_MINOR = 2.6
-export RUBY_PATCH = 2
+export RUBY_PATCH = 3
 
-export RUBY_SHA1SUM = 44c6634a41f63ebdc1f3ce6ddcf48a4766bb4df7
+export RUBY_SHA1SUM = 2347ed6ca5490a104ebd5684d2b9b5eefa6cd33c
 
 export BUNDLER_VERSION = 2.0
